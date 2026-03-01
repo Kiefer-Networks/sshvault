eecf26c26f11543ff8fbb9b0fa6f5b5363865bee
Malte Kiefer <malte.kiefer@mailbox.org>
2026-03-01 14:49:40 +0100

    chore: rename app display name to SSH Vault across all platforms
    Update app label/title in Android manifest, iOS Info.plist, Linux GTK,
    macOS config, Windows resources, and web manifest.

--- android/app/src/main/kotlin/com/shellvault/shellvault/MainActivity.kt
+++ android/app/src/main/kotlin/com/shellvault/shellvault/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.shellvault.shellvault
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
